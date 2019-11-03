b45f56f3435ea284b4a54bd1dfd0101bcdfa0506
unknown <nelsondiasdemedeiros@gmail.com>
2019-11-03 00:08:14 -0200

    chore: corrigindo bugs no urls

--- socialnetwork-api/socialnetwork/urls.py
+++ socialnetwork-api/socialnetwork/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import *
 
 urlpatterns = [
 
-    url('api/v1/profiles/', ProfileCreateOrList.as_view()),
-    url('api/v1/profiles/<int:pk>/', ProfileDetail.as_view()),
-    url('api/v1/profiles-posts/', ProfilePost.as_view()),
-    url('api/v1/profiles-posts/<int:pk>/', ProfilePostDetail.as_view()),
-    url('api/v1/posts-comments/', PostListWithComment.as_view()),
-    url('api/v1/posts-comments/<int:pk>/', PostListWithCommentDetail.as_view()),
-    url('api/v1/posts/<int:pk>/comments/', CommentCreateOrList.as_view()),
-    url('api/v1/posts/<int:pk_post>/comments/<int:pk_comment>/', CommentDetail.as_view()),
-    url('api/v1/profiles-detail/', AmountPostAndCommentFromProfile.as_view()),
-    url('', Root.as_view())
+    path('api/v1/profiles/', ProfileCreateOrList.as_view()),
+    path('api/v1/profiles/<int:pk>/', ProfileDetail.as_view()),
+    path('api/v1/profiles-posts/', ProfilePost.as_view()),
+    path('api/v1/profiles-posts/<int:pk>/', ProfilePostDetail.as_view()),
+    path('api/v1/posts-comments/', PostListWithComment.as_view()),
+    path('api/v1/posts-comments/<int:pk>/', PostListWithCommentDetail.as_view()),
+    path('api/v1/posts/<int:pk>/comments/', CommentCreateOrList.as_view()),
+    path('api/v1/posts/<int:pk_post>/comments/<int:pk_comment>/', CommentDetail.as_view()),
+    path('api/v1/profiles-detail/', AmountPostAndCommentFromProfile.as_view()),
+    path('', Root.as_view())
     
 ]
 
