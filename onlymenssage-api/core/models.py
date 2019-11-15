e654d213d41c2e635766de001fd22b651eb24dd1
unknown <nelsondiasdemedeiros@gmail.com>
2019-11-15 20:21:12 -0200

    chore: Model created profile with relations to user

--- onlymenssage-api/core/models.py
+++ onlymenssage-api/core/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
+    status = models.CharField(max_length=120)
+    
 
